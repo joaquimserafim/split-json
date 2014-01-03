4160c3a036e310bef33671705a5213bedfd8f255
Joaquim Serafim <joaquim.serafim@gmail.com>
2014-01-03 22:55:24 +0000

    change the split pattern from /\r?\n/ by /\r|\n/

--- test/index.js
+++ test/index.js
@@ -4,7 +4,14 @@ var test = require('tape');
 var JSONhandler = require('../lib/json_handler');
 var split = require('../');
 
+
 var rs = fs.createReadStream('./test/file.json');
+
+split(rs, 'ascii', function (err, data) {
+  console.log(err, data);
+});
+
+return;
  
 test('file stream', function (t) {
   t.plan(10000);
