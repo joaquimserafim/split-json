8a419062c449774c41a5bfa4d2e40511c2a2111a
joaquimserafim <joaquim.serafim@gmail.com>
2014-09-22 23:12:05 +0100

    codestyle & jshint

--- test/index.js
+++ test/index.js
@@ -3,21 +3,19 @@ var test = require('tape');
 
 var split = require('../');
 
-
-test('#stream a with single JSON data separate by "\r"', function (assert) {
+test('#stream a with single JSON data separate by "\r"', function(assert) {
   fs.createReadStream('./test/file0.json')
     .pipe(split(/\r/))
-    .on('data', function (doc) {
+    .on('data', function(doc) {
       assert.deepEqual(typeof doc, 'object');
     })
     .on('end', assert.end);
 });
 
-
-test('#stream file with a big JSON', function (assert) {
+test('#stream file with a big JSON', function(assert) {
   fs.createReadStream('./test/file1.json')
     .pipe(split(/\n,\n/, 'utf8', /(^\[\n)|(\n\]\n$)/))
-    .on('data', function (doc) {
+    .on('data', function(doc) {
       assert.deepEqual(typeof doc, 'object');
     })
     .on('end', assert.end);
