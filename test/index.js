70e2eb9fb4053963b0627a30f248da81028f656d
joaquimserafim <joaquim.serafim@gmail.com>
2014-09-24 00:20:50 +0100

    code coverage

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var test = require('tape');
+var Readable = require('stream').Readable;
 
 var split = require('../');
 
@@ -17,6 +18,20 @@ test('#stream file with a big JSON', function(assert) {
     .pipe(split(/\n,\n/, 'utf8', /(^\[\n)|(\n\]\n$)/))
     .on('data', function(doc) {
       assert.deepEqual(typeof doc, 'object');
+    })
+    .on('end', assert.end);
+});
+
+test('#a single JS Object & use split.isObject to test is a JS object',
+function(assert) {
+  var obj = {type: 2000, tmx: 0, rss: 0, heapTotal: 0,heapUsed:0};
+  var rs = new Readable();
+  rs.push(JSON.stringify(obj));
+  rs.push(null);
+
+  rs.pipe(split(/\r/))
+    .on('data', function(doc) {
+      assert.deepEqual(split.isObject(doc), true);
     })
     .on('end', assert.end);
 });
