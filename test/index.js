58056cfc926bd7498e4937adadb7a97d3944481b
quim <joaquim.serafim@gmail.com>
2015-03-20 16:50:36 +0000

    v2.0.0

--- test/index.js
+++ test/index.js
@@ -1,92 +1,92 @@
-'use strict';
+'use strict'
 
-var fs        = require('fs');
-var test      = require('tape');
-var Readable  = require('stream').Readable;
+var fs        = require('fs')
+var test      = require('tape')
+var Readable  = require('stream').Readable
 
-var split = require('../');
+var split = require('../')
 
 test('stream some bad formated json file', function(assert) {
   fs.createReadStream(__dirname + '/fixtures/some_json_stream.txt')
     .pipe(split(/\r/))
     .on('data', function(doc) {
-      assert.deepEqual(typeof doc, 'object');
+      assert.deepEqual(typeof doc, 'object')
     })
-    .on('end', assert.end);
-});
+    .on('end', assert.end)
+})
 
 test('a stream with single JSON data separate by "\\r"', function(assert) {
   fs.createReadStream(__dirname + '/fixtures/file0.json')
     .pipe(split(/\r/))
     .on('data', function(doc) {
-      assert.deepEqual(typeof doc, 'object');
+      assert.deepEqual(typeof doc, 'object')
     })
-    .on('end', assert.end);
-});
+    .on('end', assert.end)
+})
 
 test('stream file with a big JSON', function(assert) {
   fs.createReadStream(__dirname + '/fixtures/file1.json')
     .pipe(split(/\n,\n/, 'utf8', /(^\[\n)|(\n\]\n$)/))
     .on('data', function(doc) {
-      assert.deepEqual(typeof doc, 'object');
+      assert.deepEqual(typeof doc, 'object')
     })
-    .on('end', assert.end);
-});
+    .on('end', assert.end)
+})
 
 test('single JS Object & use split.isObject to test is a JS object',
 function(assert) {
-  var obj = {type: 2000, tmx: 0, rss: 0, heapTotal: 0, heapUsed:0};
-  var rs = new Readable();
-  rs.push(JSON.stringify(obj));
-  rs.push(null);
+  var obj = {type: 2000, tmx: 0, rss: 0, heapTotal: 0, heapUsed:0}
+  var rs = new Readable()
+  rs.push(JSON.stringify(obj))
+  rs.push(null)
 
   rs.pipe(split(/\r/))
     .on('data', function(doc) {
-      assert.deepEqual(split.isObject(doc), true);
+      assert.deepEqual(split.isObject(doc), true)
     })
-    .on('end', assert.end);
-});
+    .on('end', assert.end)
+})
 
 test('testing split arguments not passing the match -' +
   'match === "string"',
 function(assert) {
-  var obj = {type: 2000, tmx: 0, rss: 0, heapTotal: 0, heapUsed:0};
-  var rs = new Readable();
-  rs.push(JSON.stringify(obj));
-  rs.push(null);
+  var obj = {type: 2000, tmx: 0, rss: 0, heapTotal: 0, heapUsed:0}
+  var rs = new Readable()
+  rs.push(JSON.stringify(obj))
+  rs.push(null)
 
   rs.pipe(split('ascii'))
     .on('data', function(doc) {
-      assert.deepEqual(split.isObject(doc), true);
+      assert.deepEqual(split.isObject(doc), true)
     })
-    .on('end', assert.end);
-});
+    .on('end', assert.end)
+})
 
-test('testing split arguments - ' +
-  'passing match & replace' +
+test('testing split arguments - passing match & replace' +
   'match instanceof RegExp && typeof encoding !== "string"',
 function(assert) {
   fs.createReadStream(__dirname + '/fixtures/test_args_1.txt')
     .pipe(split(/\n/, /(^\[\n)|(\n\]\n$)/))
     .on('data', function(doc) {
-      assert.deepEqual(split.isObject(doc), true);
+      assert.deepEqual(split.isObject(doc), true)
     })
-    .on('end', assert.end);
-});
+    .on('end', assert.end)
+})
 
 test('split strings', function(assert) {
-  var i = 0;
+  var i = 0
+
   fs.createReadStream(__dirname + '/fixtures/file_strings.txt')
   .pipe(split())
   .on('data', function(data) {
-    assert.equal(typeof data, 'string');
+    assert.equal(typeof data, 'string')
     if (i++ === 0) {
-      assert.deepEqual('Hello World', data);
+      assert.deepEqual('Hello World', data)
     } else if (i === 1) {
-      assert.deepEqual('Felix the cat', data);
-      assert.end();
+      assert.deepEqual('Felix the cat', data)
+      assert.end()
     }
   })
   .on('error', assert.fail)
-  .on('end', assert.end);
-});
+  .on('end', assert.end)
+})
